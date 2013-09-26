7dc517f75f4d0127436004efbbec048f21fb2283
kashif74 <kashif.alig@gmail.com>
2013-09-26 14:37:58 +0100

    added updated version of augeas lens

--- manifests/config_augeas.pp
+++ manifests/config_augeas.pp
@@ -1,8 +1,11 @@
 define nrpe::config_augeas {
+  
+# there is a bug in default nrpe lens for aug-0.9. load_path can be removed when aug1.0 available with sl6  
 if $name != 'undef' {
   augeas { $name:
    context => "/files/etc/nagios/nrpe.cfg",
    changes => "set $name last()+1",
+   load_path => '/var/lib/puppet/lib/augeas/lenses',
    notify  =>  Service["nrpe"]
   }
  }
--- manifests/params.pp
+++ manifests/params.pp
@@ -5,10 +5,10 @@ class nrpe::params {
   $nrpe_parameters = hiera ('nrpe::params::$nrpe_parameters', [])
   
   $softwarenfs = hiera ('nrpe::params::softwarenfs', false)
-  $softwarenfs_cmd = hiera ('nrpe::params::softwarenfs_cmd', '/usr/lib64/nagios/plugins/check_softwarenfs')
+  $softwarenfs_cmd = hiera ('nrpe::params::softwarenfs_cmd', 'check_softwarenfs')
   
   $check_procs = hiera ('nrpe::params::check_procs', true)
-  $check_procs_cmd = hiera ('nrpe::params::check_procs_cmd', '/usr/lib64/nagios/plugins/check_procs -w 9 -c 17 -s Z')
+  $check_procs_cmd = hiera ('nrpe::params::check_procs_cmd', 'check_procs -w 9 -c 17 -s Z')
    
   case $::architecture {
     'x86_64', 'amd64': { $plugindir = '/usr/lib64/nagios/plugins' }
