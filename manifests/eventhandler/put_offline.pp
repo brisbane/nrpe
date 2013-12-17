e487ac1937a11961d54fdf5ab67ab3ffb03c9627
root <root@pplxconfig3.physics.ox.ac.uk>
2013-12-17 10:18:09 +0000

    Changed eventhandler dir because of sudo conflict

--- manifests/eventhandler/put_offline.pp
+++ manifests/eventhandler/put_offline.pp
@@ -1,21 +1,23 @@
 class nrpe::eventhandler::put_offline inherits nrpe::params {
   
-  $eventhadler_dir = '/usr/lib64/nagios/eventhandler'
+  # Dont want to create two sudo files
+  $eventhadler_dir = '/usr/lib64/nagios/plugins'
   augeas {do_not_blame:
   context => "/files/etc/nagios/nrpe.cfg",
   load_path => '/var/lib/puppet/lib/augeas/lenses',
   changes => " set dont_blame_nrpe 1 last()+1",
   notify  =>  Service ["nrpe"]
  }
-  
-  file { "$eventhadler_dir":
-    
-    ensure  => directory,
-    owner   => root,
-    group   => root,
-    mode    => '0755',
-    require => Package['nrpe'],
-  }
+ 
+# Eventhandler already exist 
+#  file { "$eventhadler_dir":
+#    
+#    ensure  => directory,
+#    owner   => root,
+#    group   => root,
+#    mode    => '0755',
+#    require => Package['nrpe'],
+#  }
   
    file { "${eventhadler_dir}/put_offline.sh":
     source  => 'puppet:///modules/nrpe/put_offline.sh',
@@ -25,12 +27,13 @@ class nrpe::eventhandler::put_offline inherits nrpe::params {
     require => Package['nrpe'],
   }
  
-  file { '/etc/sudoers.d/nrpe_sudo':
-    source  => 'puppet:///modules/nrpe/nrpe_sudo',
-    owner   => root,
-    group   => root,
-    mode    => '0440',
-   }
+# sudo file created by default
+#  file { '/etc/sudoers.d/nrpe_sudo':
+#    source  => 'puppet:///modules/nrpe/nrpe_sudo',
+#    owner   => root,
+#    group   => root,
+#    mode    => '0440',
+#   }
   
   file { "${nrpe::params::confd}/put_offline.cfg":
     
