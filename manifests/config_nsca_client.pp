a01bc42292b560aba999578f47777e31943108e7
kashif74 <kashif.alig@gmail.com>
2015-11-11 20:09:02 +0000

    need nagios perl utils

--- manifests/config_nsca_client.pp
+++ manifests/config_nsca_client.pp
@@ -3,10 +3,7 @@ class nrpe::config_nsca_client inherits nrpe::params {
   $password   = $nrpe::params::nsca_password
   $encryption = $nrpe::params::nsca_encryption
   
-  package {'nsca-client':
-    ensure => installed,
-  }
-  
+  ensure_packages(['nagios-plugins-perl', 'nsca-client'])   
   file {"${nsca_dir}/send_nsca.cfg":
     content => template('nrpe/send_nsca.cfg.erb'),
     owner   => root,
