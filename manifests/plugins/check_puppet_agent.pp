edc5b59669949487fb589ed9086ca00e4a7a6220
kashif74 <kashif.alig@gmail.com>
2016-06-07 11:09:43 +0100

    need ruby json for puppet check

--- manifests/plugins/check_puppet_agent.pp
+++ manifests/plugins/check_puppet_agent.pp
@@ -2,8 +2,9 @@ class nrpe::plugins::check_puppet_agent inherits nrpe::params {
  
  $check_puppet_agent_cmd = $nrpe::params::check_puppet_agent_cmd 
  # This plugin requires sudo access
-  
-  
+ 
+ #SB June 2016 puppet agent check now needs this package 
+ ensure_packages ([ruby-json])  
  file { "${nrpe::params::plugindir}/check_puppet.rb":
     
     content => template('nrpe/check_puppet.rb'),
